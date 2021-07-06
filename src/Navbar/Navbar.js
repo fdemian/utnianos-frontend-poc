238d08657cccc1c813c38bb00f7ec2887bc35c87
fcaminiti <fcaminiti@gmail.com>
2021-07-06 12:41:56 -0300

    Move navbar login logic to parent component.

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -4,25 +4,69 @@ import React, {
 } from 'react';
 import Spin from 'antd/lib/spin';
 import Affix from 'antd/lib/affix';
+import { useHistory } from 'react-router';
+import { gql, useQuery, useApolloClient } from "@apollo/client";
+import { useIsLoggedIn, useAuthToken} from '../Login/authToken';
 
 const NavbarDesktop = lazy(() => import('./NavbarDesktop'));
 const NavbarMobile = lazy(() => import('./NavbarMobile'));
+
+const GET_USER = gql`
+  query User($id: Int!) {
+    user(id: $id) {
+      id,
+      username
+      avatar
+    }
+  }
+`;
+
 
 const Navbar = (props) => {
 
   const { mobile } = props;
 
+  const history = useHistory();
+  const client = useApolloClient();
+  const { isLoggedIn } = useIsLoggedIn();
+  const [authToken, _, removeAuthToken] = useAuthToken();
+
+  const id = authToken['id'];
+  const queryOpts = { variables: { id: id }, skip: !isLoggedIn };
+  const { loading, error, data } = useQuery(GET_USER, queryOpts);
+
+  const logoutFn = async () => {
+    await client.resetStore();
+    removeAuthToken();
+    history.push(`/`);
+  }
+
+  if(error){
+    removeAuthToken();
+    history.push(`/login`);
+  }
+
+  const navProps = {
+    blogName: props.blogName,
+    notificationsEnabled: false,
+    notifications: [],
+    logoutFn: logoutFn,
+    loading: loading,
+    data: data,
+    isLoggedIn: isLoggedIn
+  };
+
   if(mobile)
     return (
     <Suspense fallback={<Spin />}>
-        <NavbarMobile {...props} />
+        <NavbarMobile {...navProps} />
     </Suspense>
     ) ;
 
     return (
     <Suspense fallback={<Spin />}>
       <Affix>
-        <NavbarDesktop {...props} />
+        <NavbarDesktop {...navProps} />
       </Affix>
     </Suspense>
   );
--- src/Navbar/NavbarDesktop.js
+++ src/Navbar/NavbarDesktop.js
@@ -5,27 +5,14 @@ import React, {
 import { Menu, Spin } from 'antd';
 import NavLogo from './NavLogo';
 import { Link } from 'react-router-dom';
-import { useHistory } from 'react-router';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faSignInAlt as signIn
 } from '@fortawesome/free-solid-svg-icons';
-import { gql, useQuery, useApolloClient } from "@apollo/client";
-import { useIsLoggedIn, useAuthToken} from '../Login/authToken';
 import './Navbar.css';
 
 const AccountMenu = lazy(() => import('./AccountMenu/AccountMenu'));
 //const Notifications = lazy(() => import('./Notifications'));
-
-const GET_USER = gql`
-  query User($id: Int!) {
-    user(id: $id) {
-      id,
-      username
-      avatar
-    }
-  }
-`;
 
 const Navbar = (props) => {
 
@@ -36,30 +23,14 @@ const Navbar = (props) => {
     markReadNotification,
     dismissNotifications,
     */
+    loading,
+    data,
+    logoutFn,
+    isLoggedIn,
     blogName
   } = props;
 
-  const history = useHistory();
-  const client = useApolloClient();
-  const { isLoggedIn } = useIsLoggedIn();
-  const [authToken, _, removeAuthToken] = useAuthToken();
-
-  const id = authToken['id'];
-  const queryOpts = { variables: { id: id }, skip: !isLoggedIn };
-  const { loading, error, data } = useQuery(GET_USER, queryOpts);
-
-  const logoutFn = async () => {
-    await client.resetStore();
-    removeAuthToken();
-    history.push(`/`);
-  }
-
-  if(error){
-    removeAuthToken();
-    history.push(`/login`);
-  }
-
-  if(error || !isLoggedIn)
+  if(!isLoggedIn)
   {
     return(
     <Suspense fallback={<Spin />}>
