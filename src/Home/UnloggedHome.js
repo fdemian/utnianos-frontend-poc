996e2587189ca974c519bd23fad936fa6f5a8372
fcaminiti <fcaminiti@gmail.com>
2021-07-07 14:15:11 -0300

    Fixes to landing page.

--- src/Home/UnloggedHome.js
+++ src/Home/UnloggedHome.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css';
+import logo from '../logo.png';
 
 const UnloggedHome = () => {
   return (
-    <div>
-      <h1> UNLOGGED HOME </h1>
-    </div>
+  <div className="unlogged-home-container">
+    <img src={logo} alt="utnianos logo" />
+    <p style={{fontSize:'30px'}}>Bienvenido!</p>
+    <p style={{fontSize:'20px'}}>
+      En este sitio vas a poder seguir tu progreso académico, ver apuntes y más.
+    </p>
+    <p style={{fontSize:'20px'}}>
+      <Link to="/login">Inicia sesión</Link> o <Link to="/register">registrate</Link> si no tenés un usuario.
+    </p>
+  </div>
   );
 }
 
