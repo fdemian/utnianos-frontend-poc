1d90663fb045c34636c53f5a26d63ae8d20d88c5
fcaminiti <fcaminiti@gmail.com>
2021-07-18 13:19:13 -0300

    File view page.

--- src/FileView/FileView.js
+++ src/FileView/FileView.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { gql } from "@apollo/client";
+import { Spin, Tag } from 'antd';
+import { gql, useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
+import './FileView.css';
 
-// get Class material file.
-export const GET_FILE = gql`
-  query GetContributions {
-    classMaterials {
-      id
+const GET_COURSE_MATERIAL = gql`
+  query GetCourseMaterial($id: Int!) {
+    classMaterial(id: $id) {
       name
-      filePath
+      description
       contribTypes
       course {
         id
@@ -18,14 +18,41 @@ export const GET_FILE = gql`
   }
 `;
 
-const FileView = ({}) => {
+const FileView = () => {
   const params = useParams();
   const { id } = params;
-  
+
+  const { loading, error, data } = useQuery(GET_COURSE_MATERIAL, {
+      variables: { id: id }
+    });
+
+  if(loading)
+    return <Spin tip="Loading" />;
+
+  if(error)
+    return <p>Error</p>;
+
+  const { classMaterial } = data;
+
+  const {
+   name,
+   contribTypes,
+   course,
+   description,
+  } = classMaterial;
+
   return (
-  <div>
-    {id}
-  </div>
+  <>
+    <h1 className="class-material-title">{name}</h1>
+    <h2 className="center-margin">{course.name}</h2>
+    <h3 className="center-margin">{description}</h3>
+    <p className="center-margin">Tipos de contribución</p>
+    <span className="center-margin">
+    {contribTypes.split(',').map(t => (
+      <Tag key={t}>{t}</Tag>
+    ))}
+    </span>
+  </>
   );
 }
 
